31be67e4d555960390780ac2191c7f9e55347f89
kiran <kiranjadhav9561@gmail.com>
2022-12-22 19:09:05 -0800

    done product listing

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
       <footer className="text-center text-lg-start bg-white text-muted">
         <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
           <div className="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
+            {/* <span>Get connected with us on social networks:</span> */}
           </div>
 
           <div>
@@ -36,59 +36,59 @@ function Footer() {
             <div className="row mt-3">
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">
-                  <i className="fas fa-gem me-3 text-secondary"></i>Company name
+                  <i className="fas fa-gem me-3 text-secondary"></i>Zip Cart
                 </h6>
                 <p>
-                  Here you can use rows and columns to organize your footer
-                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                  elit.
+                  Zip kart Internet Private Limited, Buildings Alyssa, Begonia &
+                  Clove Embassy Tech Village, Outer Ring Road,
+                  Devarabeesanahalli Village, Pune, 560103, Maharashtra, India
                 </p>
               </div>
 
               <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+                <h6 className="text-uppercase fw-bold mb-4">Help</h6>
                 <p>
                   <a href="#!" className="text-reset">
-                    Angular
+                    Payment
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    React
+                    Shiping
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Vue
+                    FAQ
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Laravel
+                    Report
                   </a>
                 </p>
               </div>
 
               <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                <h6 className="text-uppercase fw-bold mb-4">Social</h6>
                 <p>
                   <a href="#!" className="text-reset">
-                    Pricing
+                    Facebbok
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Settings
+                    Twitter
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Orders
+                    YouTube
                   </a>
                 </p>
                 <p>
                   <a href="#!" className="text-reset">
-                    Help
+                    LinkedIn
                   </a>
                 </p>
               </div>
@@ -96,20 +96,20 @@ function Footer() {
               <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                 <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                 <p>
-                  <i className="fas fa-home me-3 text-secondary"></i> New York,
-                  NY 10012, US
+                  <i className="fas fa-home me-3 text-secondary"></i> Pune, MH
+                  10012, India
                 </p>
                 <p>
                   <i className="fas fa-envelope me-3 text-secondary"></i>
-                  info@example.com
+                  info@zipcart.com
                 </p>
                 <p>
-                  <i className="fas fa-phone me-3 text-secondary"></i> + 01 234
-                  567 88
+                  <i className="fas fa-phone me-3 text-secondary"></i> + 91 934
+                  567 8800
                 </p>
                 <p>
-                  <i className="fas fa-print me-3 text-secondary"></i> + 01 234
-                  567 89
+                  <i className="fas fa-print me-3 text-secondary"></i> + 91 923
+                  567 8954
                 </p>
               </div>
             </div>
@@ -117,9 +117,9 @@ function Footer() {
         </section>
 
         <div className="text-center p-4 background">
-          © 2021 Copyright:
+          © 2022 Copyright:
           <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-            MDBootstrap.com
+            ZipCart.com
           </a>
         </div>
       </footer>
--- src/component/ProductList.js
+++ src/component/ProductList.js
@@ -9,23 +9,33 @@ function ProductList() {
   }, []);
 
   const getProducts = async () => {
-    let result = await fetch("http://localhost:4001/products");
+    let result = await fetch("http://localhost:4000/products");
     result = await result.json();
     setProducts(result);
   };
 
   console.warn("product", products);
   return (
-    <div>
-      {products.map((item, index) => (
-        <ul>
-          <li>{index + 1}</li>
-          <li>item.name</li>
-          <li>{item.price}</li>
-          <li>{item.category}</li>
-          <li>{item.company}</li>
+    <div className="product-list">
+      <h3 className="text-center mt-4">Product List</h3>
+      <div className="container">
+        <ul id="first-ul">
+          <li>Sr No</li>
+          <li>Name</li>
+          <li>Price</li>
+          <li>Category</li>
+          <li>Company</li>
         </ul>
-      ))}
+        {products.map((item, index) => (
+          <ul>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>{item.price}</li>
+            <li>{item.category}</li>
+            <li>{item.company}</li>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
